4595bc99cb32876ee32bd2f9fbdf4e8c6aaa04dc
racibih <rachel.ibihwiori@bison.howard.edu>
2021-09-23 19:37:29 -0400

    added comment

--- LSP_Ibihwiori/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Ibihwiori/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -3,8 +3,9 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//a greeting
 		System.out.println("Hello World");
+		
 		
 	}
 
