6a24b94e8cbb47e4c3e8a940fc95c6f15d79d1f6
racibih <rachel.ibihwiori@bison.howard.edu>
2022-01-26 21:33:11 -0500

    intial commit

--- LSP_Ibihwiori/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Ibihwiori/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -3,8 +3,8 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		//a greeting
-		System.out.println("Hello World");
+		
+		System.out.println("Hello World!!");
 		
 		
 	}
